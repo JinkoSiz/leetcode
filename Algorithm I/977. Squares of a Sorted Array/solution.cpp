6a132b0f9f87255ef1e5122dec7cad93e40d3e17
Zakhar Sotnichenko <108886405+JinkoSiz@users.noreply.github.com>
2022-08-25 19:55:14 +0300

    Create solution.cpp

---
+++ Algorithm I/977. Squares of a Sorted Array/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int l=0, r=nums.size()-1;
+        vector<int> res(nums.size());
+        for(int i=nums.size()-1;i>=0;i--){
+            if(abs(nums[l])>abs(nums[r])){
+                res[i]=nums[l]*nums[l++];
+            }
+            else{
+                res[i]=nums[r]*nums[r--];
+            }
+        }
+        return res;
+    }
+};
